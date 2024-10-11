432ac66e26c49a33777f3c8ff6a7aedb9b65252f
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-10-11 10:38:00 -0300

    feat: fix exibition positions and add notifications

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,6 +3,8 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Proposals extends Component
@@ -10,6 +12,33 @@ class Proposals extends Component
 {
     public Project $project;
 
+    public int $qtd = 5;
+
+    #[Computed()]
+    public function proposals()
+    {
+
+        return $this->project->proposals()
+            ->orderBy('hours')
+            ->paginate($this->qtd);
+    }
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project
+            ->proposals()
+            ->latest()
+            ->first()
+            ->created_at
+            ->diffForHumans();
+    }
+
+    public function loadMore()
+    {
+        $this->qtd += 5;
+    }
+
+    #[On('proposal::created')]
     public function render()
     {
         return view('livewire.projects.proposals');
