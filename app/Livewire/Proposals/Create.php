0595afecff00a8a09ba4e29faffb115f581cb9dd
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-10-11 16:57:09 -0300

    feat: add support to send mail notifications

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -5,6 +5,8 @@ namespace App\Livewire\Proposals;
 use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
+use App\Notifications\NewProposal;
+use App\Notifications\PerdeuMane;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Rule;
 use Livewire\Attributes\Validate;
@@ -51,13 +53,16 @@ class Create extends Component
 
 
             $this->dispatch('proposal::created');
-
-            $this->modal = false;
         });
+
+        $this->project->author->notify(new NewProposal($this->project));
+
+        $this->modal = false;
     }
 
     public function arrangePositions(Proposal $proposal)
     {
+
         $query = DB::select("
             SELECT 
                 *,
@@ -75,7 +80,10 @@ class Create extends Component
             $oProposal = Proposal::find($otherProposal->id);
 
             $oProposal->update(['position_status' => 'down']);
+            $oProposal->notify(new PerdeuMane($this->project));
         }
+
+
 
         ArrangePositions::run($proposal->project_id);
     }
